bdae6023d526515f144bbf987610da4cd4772b53
Daman2461 <142651012+Daman2461@users.noreply.github.com>
2024-09-07 18:46:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import numpy as np
+import tensorflow as tf
+
+from recsys_utils import *
+import pandas as pd
+import streamlit as st
+import random
+
+
+st. set_page_config(layout="wide")
+st.write(""" <h1> <b style="color:red"> Movie Reccomender System</b> </h1>""",unsafe_allow_html=True)
+st.write(""" <h7> <b style="color:white"> Uses Content Based Filtering</b> </h7>""",unsafe_allow_html=True)
+st.write(""" <h7> <b style="color: white"> Made by Damanjit</b> </h7>""",unsafe_allow_html=True)
+ 
+List = readCSV('./top10K-TMDB-movies.csv')
+movieList = List['title']
+
+st.session_state.options = []
+st.session_state.id = []
+with st.form("my_form"):
+    options = []
+    for i in range(3):
+        option = st.selectbox(
+            "Enter your Most Favorite Movies",
+            movieList,
+            key=f'selectbox_{i}',placeholder="",index=None
+        )
+        options.append(option)
+    
+    
+    submitted = st.form_submit_button('Submit my picks')
+    if submitted:
+        st.session_state.options = options
+        
+
+st.write(""" <h1> <b style="color:red"> Recommendations</b> </h1>""",unsafe_allow_html=True)
+
+
+if submitted:
+        
+    mov = findSimilar(st.session_state.options)
+    columns = st.columns(5)
+    m_id = List[List['title'].isin(mov)]['id']
+    for i,col in zip(m_id[:5],columns):
+        
+        col.image(poster(i),caption = List[List['id'] == i][ 'title'].values,width=200)
+
+    
+    
+    
+
+for i,col in zip(m_id[5:],columns):
+        
+        col.image(poster(i),caption = List[List['id'] == i][ 'title'].values,width=200)
---
+++ recsys_utils.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+import streamlit as st
+import numpy as np
+import requests
+
+
+def readCSV(file):
+    return pd.read_csv(file)
+
+def findSimilar(movies3):
+     
+     
+    
+    a = pd.DataFrame((cosine_sim[indices[movies3[0]]]+cosine_sim[indices[movies3[1]]]+cosine_sim[indices[movies3[2]]])/3, columns=["score"])
+    
+    
+    movie_indices = [indices[movies3[0]], indices[movies3[1]], indices[movies3[2]]]
+    
+    
+    a = a.drop(movie_indices)
+    similar = a.sort_values("score", ascending=False)[1:11].index
+    return (movies['title'].iloc[similar])
+   
+
+
+def poster(moveid):
+   url='https://api.themoviedb.org/3/movie/{}?api_key=f2f4840944d9cc178e9f22d1979eb440'.format(moveid)
+   data = requests.get(url)
+   json =data.json()
+
+   pic_path = json['poster_path']
+
+   full_path = "https://image.tmdb.org/t/p/w500/" + pic_path
+   return full_path
+
+
+
+
+movies = pd.read_csv("./top10K-TMDB-movies.csv") 
+
+
+movies.dropna(inplace=True)
+ 
+movies.isnull().sum()
+movies.reset_index(drop=True, inplace=True)
+
+movies['combined_text'] = movies['title']+" "+movies['overview']
+
+tfidf = TfidfVectorizer(stop_words='english')  # Initialize TF-IDF Vectorizer
+tfidf_matrix = tfidf.fit_transform(movies['combined_text'])  
+
+ 
+tfidf_matrix.toarray()
+
+
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+indices = pd.Series(movies.index, index=movies['title'])
+indices = indices[~indices.index.duplicated(keep='last')]
+
+ 
+
+
