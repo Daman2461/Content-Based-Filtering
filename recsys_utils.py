8978524d9abb16b5f8e334c9e835fba7a90f524e
Daman2461 <142651012+Daman2461@users.noreply.github.com>
2024-09-07 18:59:18 +0530

    Update recsys_utils.py

--- recsys_utils.py
+++ recsys_utils.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 import streamlit as st
